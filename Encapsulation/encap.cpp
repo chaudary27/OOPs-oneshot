b68dcc741d2694c31c8ca838d76e8a4c35f99910
Farhan Saleem <chaudaryfarhann@gmail.com>
2025-05-26 11:36:41 +0500

    Encapsulation added

---
+++ Encapsulation/encap.cpp
@@ -0,0 +1,16 @@
+// encapsulation 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class accounts{
+	private:
+		double balance; // data hiding 
+		string password;
+	
+	public:
+		string user_id;
+		string username;
+
+};

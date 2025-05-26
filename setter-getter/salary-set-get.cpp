f659cac829cbd59802b7a78cd850f8bc86a95df8
Farhan Saleem <chaudaryfarhann@gmail.com>
2025-05-26 11:38:01 +0500

    Setters-Getters

---
+++ setter-getter/salary-set-get.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using  namespace std;
+
+class Teacher{
+	
+	private:
+		double salary;
+
+	public:
+		string name;
+		string dept;
+		string subject;
+		
+	// setter
+	void set_salary(double new_salary){
+		salary=new_salary;
+	}
+	
+	// getter
+	double get_salary(){
+		return 	salary;
+	}
+	
+};
+
+int main(){
+	Teacher t1;
+	t1.name="ali";
+	t1.dept="CS";
+	t1.subject="PF";
+	t1.set_salary(25000);
+	
+	cout<<t1.name<<endl;
+	cout<<t1.subject<<endl;
+	cout<<t1.dept<<endl;
+	cout<<t1.get_salary();
+	
+	return 0;
+}
